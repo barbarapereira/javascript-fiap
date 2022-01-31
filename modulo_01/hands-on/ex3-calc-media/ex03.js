c83ff88cba2589f092665b4e035d018b5a96bbe6
Barbara Pereira <barbara.pereira1990@gmail.com>
2022-01-31 10:15:19 -0300

    correçao exercicio media, /3

--- modulo_01/hands-on/ex3-calc-media/ex03.js
+++ modulo_01/hands-on/ex3-calc-media/ex03.js
@@ -2,4 +2,4 @@ let ac = parseFloat(prompt('Entre com a nota AC: '));
 let ag = parseFloat(prompt('Entre com a nota Avaliação Geral: '));
 let at = parseFloat(prompt('Entre com a nota Avaliação Trimestral: '));
 
-alert(`A Média do aluno(a) é: ${(((ac*0.20) + (ag *0.10) + (at * 0.70))/3).toFixed(2)}`)
+alert(`A Média do aluno(a) é: ${((ac*0.20) + (ag *0.10) + (at * 0.70)).toFixed(2)}`)
