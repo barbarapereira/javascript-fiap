4f3b4b476abec6b0f163f15610f29bd919cc7384
Barbara Pereira <barbara.pereira@gmail.com>
2022-04-30 13:04:25 -0300

    aula 2 - parte 1

---
+++ modulo_04_react/fiap_shift_react/src/Pages/Home/HomeController.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import HomeView from './HomeView';
+
+export default class HomeController extends Component {
+  render() {
+    return (
+      <HomeView/>
+    );
+  }
+}
---
+++ modulo_04_react/fiap_shift_react/src/Pages/Home/HomeView.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+function HomeView() {
+    return (
+        <div className="App">Hello World HomeView</div>
+    );
+};
+
+export default HomeView;
